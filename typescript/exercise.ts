0c9da6175bf291f229af63087a71e8b9106c3235
Sonal Maheshwari <sonal.obedient@gmail.com>
2026-01-29 11:58:56 +0100

    updated in codes

--- typescript/exercise.ts
+++ typescript/exercise.ts
@@ -1,11 +1,37 @@
 //Enums (fixed list of options) -
-//1. why my output comes 2, i am not using any index number here ??
+//1. why my output comes in number, i am not using any findIndex function here ??
 
-enum Color {Red, Green , Blue};
-const showColor= (color: Color) => {
-    console.log("you chose:", color);
+enum Color { Red, Green, Blue };
+const showColor = (color: Color) => {
+    console.log("You chose:", color);
 };
 showColor(Color.Blue);
 
 //2.
 
+enum PizzaSize {
+    Small,
+    Medium,
+    Large
+}
+const orderPizza = (size: PizzaSize) => {
+    console.log(`You ordered a ${size} pizza. `)
+}
+orderPizza(PizzaSize.Medium);
+
+
+//3. 
+enum Role {
+    Admin, User, Guest
+}
+const printRole = (check: Role) => {
+    if (check === Role.Admin) {
+        console.log("You have full access.");
+    } else if (check === Role.User) {
+        console.log("You have limited access.");
+    } else if (check === Role.Guest) {
+        console.log("You have guest access.");
+    }
+};
+printRole(Role.Admin);
+printRole(Role.Guest);
